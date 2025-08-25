8cee44f7810da91997b79c20942d951a308fd332
alfiandi-dev <aldialfiandi2708@gmail.com>
2025-08-25 11:04:59 +0700

    init

--- src/pages/Event/index.js
+++ src/pages/Event/index.js
@@ -1,6 +1,5 @@
 // get all the book functions
 import EventPartyAdd from "./EventPartyAdd";
 import EventList from "./EventList";
-
 // export all of the book functions
 export { EventList, EventPartyAdd };
